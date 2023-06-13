9698692e95a34b9430c6285e55e71e04a58df80e
roj21008 <roj21008@byui.edu>
2023-06-12 19:50:40 -0500

    500

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongodb = require('./data/database');
 
 const app = express();
 
-const port = process.env.PORT || 8550;
+const port = process.env.PORT || 500;
 
 app.use('/',require('./routes'));
 
